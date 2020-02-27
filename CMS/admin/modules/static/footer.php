e441348a14f6885442774704cb628adb5aaedcfb
Paweł Wnuk Lipinski <wnuklipinski95@gmail.com>
2020-02-27 22:33:50 +0100

    [CMS] Utworzenie systemu CMS #6
    Add footer and header module

---
+++ CMS/admin/modules/static/footer.php
@@ -0,0 +1,34 @@
+<?php
+function footerAdmin(){
+    ?>
+    <div class="afficite-height"></div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <p>Copyright (c) 2018 by Mateusz Bartelik</p>
+            </div>
+            <div class="col-lg-6 right">
+                <address>
+                    <span class="glyphicon glyphicon-earphone"></span>
+                    <p>792 692 947</p>
+                </address>
+                <address>
+                    <span class="glyphicon glyphicon-envelope"></span>
+                    <p>mateusz.bartelik@wp.eu</p>
+                </address>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="js/jquery.min.js"></script>
+<script defer src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
+<?php
+}
+?>
